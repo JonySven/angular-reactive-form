398ac4c23bec26981fab6b285ea1b73bff02fcee
Imanov <e.imanov@solarl.ru>
2023-01-27 09:37:02 +0300

    angular-reactive-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'form';
+export class AppComponent implements OnInit {
+  forma! : FormGroup;
+  get commentControls(): AbstractControl[]{
+    return (this.forma.get('comment') as FormArray).controls
+  }
+  ngOnInit(): void {
+    this.forma = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl('',[Validators.minLength(6)]),
+      message: new FormGroup({
+        from: new FormControl(''),
+        text: new FormControl('')
+      }),
+      comment: new FormArray([])
+    });
+    this.forma.valueChanges.subscribe();
+  }
+
+  addComment(){
+    const control = new FormControl('');
+    (this.forma.get('comment') as FormArray).push(control)
+  }
+
+  qwe: any
+  submit() {
+    const valueForm = { ...this.forma.value}
+    this.qwe= valueForm
+  }
 }
